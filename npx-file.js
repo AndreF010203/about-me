ac9b1542c16f7471c931c01a2483419580525c03
Andrea <andrea.facchini.io@gmail.com>
2021-02-13 18:38:52 +0100

    fix repo name

--- npx-file.js
+++ npx-file.js
@@ -8,4 +8,4 @@ const url = 'https://github.com/AndreF010203/about-me.git';
 spawnSync('git', ['clone', url]);
 spawnSync('npm', ['install']);
 
-console.log('About-me has been successfully installed');
+console.log('About-me has been successfully installed in the about_me folder');
